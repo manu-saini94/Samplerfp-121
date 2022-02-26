4de8d338a1e459efa175b20e59423e772732bd9b
manu-saini94 <57988195+manu-saini94@users.noreply.github.com>
2022-02-26 10:01:56 +0530

    Update Sample1.java
    first commit to slave-3 by developer - 2

--- src/com/bridgelabz/empwage/Sample1.java
+++ src/com/bridgelabz/empwage/Sample1.java
@@ -7,5 +7,6 @@ public class Sample1  {
       Sample1 sample1 = new Sample1();
       Sample1 sample2 = new Sample1(); // this line added in slave-1 branch
       Sample1 sample3 = new Sample1(); // this line added in slave-2 branch
+      Sample1 sample4 = new Sample1(); // this line added in slave-3 branch by developer - 2  
     }
 }
