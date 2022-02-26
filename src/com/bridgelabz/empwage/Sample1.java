07a7c8752b9ba7252f6620d806d314e18a8104a4
Manu Saini <manu.s@perfios.com>
2022-02-26 10:18:04 +0530

    conflict resolved

--- src/com/bridgelabz/empwage/Sample1.java
+++ src/com/bridgelabz/empwage/Sample1.java
@@ -9,5 +9,7 @@ public class Sample1  {
       Sample1 sample3 = new Sample1(); // this line added in slave-2 branch
       Sample1 sample4 = new Sample1(); // this line added in slave-3 branch by developer - 2  
       Sample1 sample4 = new Sample1(); // this Line 2 added in slave-3 branch by developer - 2 
+      Sample1 sample5 = new Sample1(); // this line added in slave-3 branch by developer - 1  
+
     }
 }
