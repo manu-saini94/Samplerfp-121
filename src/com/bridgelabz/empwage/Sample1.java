bc008b6902da19823ab7bc3d1cad4bf59b770cb9
manu-saini94 <57988195+manu-saini94@users.noreply.github.com>
2022-02-28 08:25:24 +0530

    change by developer-3

--- src/com/bridgelabz/empwage/Sample1.java
+++ src/com/bridgelabz/empwage/Sample1.java
@@ -6,6 +6,7 @@ public class Sample1  {
 
       Sample1 sample1 = new Sample1();
       Sample1 sample2 = new Sample1(); // this line added in slave-1 branch
+      Sample1 sample6 = new Sample1(); // change done by developer-3
 
     }
 }
